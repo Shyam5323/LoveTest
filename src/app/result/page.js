d86b5d1d86a70801ddb5b49315baecc38e030c8a
chandan singh <chandan.singh2022@vitstudent.ac.in>
2024-02-24 20:17:42 +0530

    Loading Screen and images

--- src/app/result/page.js
+++ src/app/result/page.js
@@ -1,34 +1,54 @@
-"use client"
-import { useEffect,useState } from "react";
-import "./result.css"
+"use client";
+import { useEffect, useState } from "react";
+import "./result.css";
 export default function Result() {
-    const [showText, setShowText] = useState(false);
-    const [showText1, setShowText1] = useState(false);
-  
-    useEffect(() => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (isLoading) {
       const timeout = setTimeout(() => {
-        setShowText(true);
-      }, 4000); 
-        return () => clearTimeout(timeout);
-    }, []); 
-    useEffect(() => {
-      const timeout = setTimeout(() => {
-        setShowText1(true);
-      }, 2000); 
-        return () => clearTimeout(timeout);
-    }, []); 
+        setIsLoading(false); 
+      }, 3000);
+      return () => clearTimeout(timeout);
+    }
+  }, [isLoading]);
+  const [showText, setShowText] = useState(false);
+  const [showText1, setShowText1] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowText(true);
+    }, 4000);
+    return () => clearTimeout(timeout);
+  }, []);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowText1(true);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, []);
   return (
     <div className="flex flex-col justify-center items-center h-screen mb-40">
-      <h1 className=" font-bold text-2xl ">Remember</h1>
-      <h1 className={`text-xl font-semibold fade ${showText1 ? 'fade-in' : 'hidden'}`}>
-        The 4th step to getting a girl is to</h1>
-      <h1 className={`text-xl font-semibold ${showText ? 'fade-in' : 'hidden'}`}>
-        Follow your passion
-      </h1>  
+{isLoading ? (
+        <div className="loading-screen">
+          <p>Calculating your Love Test score...</p>
+          <progress className="progress w-56"></progress>
+        </div>
+      ) : (
+        <>
+          <h1 className="font-bold text-2xl">Remember</h1>
+          <h1 className={`text-xl font-semibold fade ${showText1 ? "fade-in" : "hidden"}`}>
+            The 4th step to getting a girl is to
+          </h1>
+          <h1 className={`text-xl font-semibold ${showText ? "fade-in" : "hidden"}`}>
+            Follow your passion
+          </h1>
           <video width="320" height="240" controls preload="none" autoPlay>
-        <source src="/nhpadhai.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+            <source src="/nhpadhai.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </>
+      )}
+      </div>
   );
 }
--- src/components/QuizHero.js
+++ src/components/QuizHero.js
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { useState } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Link from "next/link";
@@ -8,6 +9,7 @@ export default function QuizHero({ data }) {
   const [quesIndex, setQuesIndex] = React.useState(0);
   const [selectedOption, setSelectedOption] = React.useState(null);
   const currentQuestion = data[quesIndex];
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleNext = () => {
     if (selectedOption === null) {
@@ -24,8 +26,15 @@ export default function QuizHero({ data }) {
     setSelectedOption(optionIndex);
   };
 
-  const showResult = () => {};
-
+  const showResult = () => {
+    setIsLoading(true);
+    // Simulate loading for 3 seconds
+    setTimeout(() => {
+      setIsLoading(false);
+      // Redirect to the result page after 3 seconds
+      window.location.href = "/result";
+    }, 3000);
+  };
   return (
     <main>
       <p className="font-light">{`Question ${quesIndex + 1}/15`}</p>
@@ -63,6 +72,86 @@ export default function QuizHero({ data }) {
           </button>{" "}
         </Link>
       )}
+      <div className="flex justify-center">
+        {quesIndex === 1 || quesIndex === 2 ? (
+          <div className="flex flex-col">
+            <p className="text-xl font-light text-center"> It's inside this box</p>
+            <img src="/present.webp" alt="Present" />
+          </div>
+        ) : quesIndex === 3 ? (
+          <div className="flex flex-col">
+            <p  className="text-xl font-light text-center"> Let's open the gift</p>
+            <img src="/present2.webp" alt="Present" />
+          </div>
+        ) : quesIndex === 0 ? (
+          <p className="text-5xl text-center font-light mt-10">
+            We've got a gift for you..
+          </p>
+        ) : quesIndex === 4 ? (
+          <div className="flex flex-col">
+            <p  className="text-xl font-light text-center"> Something's happening , It's almost out</p>
+            <img src="/present3.avif" alt="Present" />
+          </div>
+        ) : quesIndex === 5 ? (
+          <div className="flex flex-col">
+            <p  className="text-xl font-light text-center"> Drumrolls please</p>
+            <img src="/present4.jpg" alt="Present" />
+          </div>
+        ) : quesIndex === 6 ? (
+          <div className="flex flex-col">
+            <p  className="text-xl font-light text-center">
+              
+              Ta-da <br></br> It's Gwen
+            </p>
+            <img src="/gwen1.jpg" alt="Present" />
+          </div>
+        ) : quesIndex === 7 ? (
+          <div className="flex flex-col">
+            <p  className="text-xl font-light text-center"> In case that wasnt HD enough for you</p>
+            <img src="/gwen2.jpeg" alt="Present" />
+          </div>
+        ) : quesIndex === 8 ? (
+          <div className="flex flex-col">
+            <p  className="text-xl font-light text-center"> Well, it's getting a bit steamy in here</p>
+            <img src="/gwen3.jpg" alt="Present" />
+          </div>
+        ) : quesIndex === 9 ? (
+          <div className="flex-col">
+            <p  className="text-xl font-light text-center"> For all the rajasthanis there</p>
+            <img src="/gwen4.png" alt="Present" />
+          </div>
+        ) : quesIndex === 10 ? (
+          <div className="flex flex-col">
+            <p  className="text-xl font-light text-center">It's kind of getting out of hand now</p>
+            <img src="/gwen5.jpg" alt="Present" />
+          </div>
+        ) : quesIndex === 11 ? (
+          <div className="flex flex-col">
+            <p  className="text-xl font-light text-center">
+              {" "}
+              uh-uh<br></br> That's it
+            </p>
+            <img src="/gwen6.png" alt="Present" />
+          </div>
+        ) : quesIndex === 12 ? (
+          <img src="/nohorny1.jpeg" alt="Some other image" />
+        ) : quesIndex === 13 ? (
+          <div className="flex flex-col">
+            <p  className="text-xl font-light text-center">It is a love test we're giving here. Gotta stop</p>
+            <video width="320" height="240" controls preload="none" autoPlay>
+              <source src="/nohorny2.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        ) : quesIndex === 14 ? (
+          <div className="flex flex-col">
+            <p  className="text-xl font-light text-center"> Pro Tip : Don't Cheat guys</p>
+            <img src="/noenemy.jpg" alt="Present" />
+          </div>
+        ) : (
+          <p></p>
+        )}
+      </div>
     </main>
   );
 }
